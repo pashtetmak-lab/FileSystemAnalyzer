efbc1c7cca820de3572cfe5c78cfcdb805a16d31
pashtetmak-lab <pashtetmak@gmail.com>
2026-03-06 15:42:39 +0300

    Update SizeCalculator.cpp

--- src/SizeCalculator.cpp
+++ src/SizeCalculator.cpp
@@ -1,1 +1,22 @@
+#include "../include/SizeCalculator.h"
 
+std::uintmax_t SizeCalculator::Calculate(FsNode& node) const
+{
+    if (node.isFile())
+    {
+        return node.file().size_bytes;
+    }
+
+    auto& dir = node.directory();
+
+    std::uintmax_t sum = 0;
+
+    for (auto& child : node.children())
+    {
+        sum += Calculate(*child);
+    }
+
+    dir.total_size = sum;
+
+    return sum;
+}
