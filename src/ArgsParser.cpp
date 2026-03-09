470830282392c6b29be05bf7959b3b0b05513fb5
pashtetmak-lab <pashtetmak@gmail.com>
2026-03-09 10:45:50 +0300

    Implement argument parsing in ArgsParser

--- src/ArgsParser.cpp
+++ src/ArgsParser.cpp
@@ -1,1 +1,27 @@
+#include "../include/ArgsParser.h"
 
+#include <string>
+
+FileSystemScanner::ScanConfig
+ArgsParser::Parse(int argc, char* argv[]) const
+{
+    FileSystemScanner::ScanConfig config;
+
+    if (argc >= 2)
+    {
+        config.root = argv[1];
+    }
+
+    for (int i = 2; i < argc; ++i)
+    {
+        std::string arg = argv[i];
+
+        if (arg == "--depth" && i + 1 < argc)
+        {
+            config.max_depth = std::stoi(argv[i + 1]);
+            ++i;
+        }
+    }
+
+    return config;
+}
