55fb8a6335d1ca58422f443f9a9f7849e70f2959
Shilov Vladimir <gromozev8847@gmail.com>
2026-03-10 21:59:42 +0300

    FsNode test

--- tests/test_fs_node.cpp
+++ tests/test_fs_node.cpp
@@ -1,1 +1,26 @@
-// Placeholder for FsNode unit tests.
+#include "../include/FsNode.h"
+
+#include <cassert>
+#include <memory>
+
+void TestNodeType()
+{
+    FsNode file_node(FsNode::FileInfo{"file.txt", 10});
+    FsNode dir_node(FsNode::DirInfo{"root", 0});
+
+    assert(file_node.isFile());
+    assert(!file_node.isDirectory());
+    assert(dir_node.isDirectory());
+    assert(!dir_node.isFile());
+}
+
+void TestAddChild()
+{
+    FsNode parent(FsNode::DirInfo{"root", 0});
+    auto child = std::make_unique<FsNode>(FsNode::FileInfo{"a.txt", 10});
+
+    parent.addChild(std::move(child));
+
+    assert(parent.children().size() == 1);
+    assert(parent.children()[0]->isFile());
+}
