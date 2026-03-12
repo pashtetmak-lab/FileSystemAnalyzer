69978631db83e5c2e65bd472e14223d42c3fe07e
Shilov Vladimir <gromozev8847@gmail.com>
2026-03-12 12:28:12 +0300

    файл с запуском всех тестов test_main.cpp, readme update

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 
+// Все файлы собираются в один общий исполняемый
+// файл unit_tests
+
+void TestParseDefaultArgs();
+void TestParseDepth();
+void TestRejectNegativeDepth();
+void TestScanReturnsRoot();
+void TestScanRespectsDepth();
+void TestNodeType();
+void TestAddChild();
+void TestBuildTreeReturnsRoot();
+void TestBuildTreeCreatesChildren();
+void TestBuildTreeReturnsNullForEmptyInput();
+
 int main()
 {
-    std::cout << "unit_tests placeholder" << std::endl;
+    TestParseDefaultArgs();
+    TestParseDepth();
+    TestRejectNegativeDepth();
+    TestScanReturnsRoot();
+    TestScanRespectsDepth();
+    TestNodeType();
+    TestAddChild();
+    TestBuildTreeReturnsRoot();
+    TestBuildTreeCreatesChildren();
+    TestBuildTreeReturnsNullForEmptyInput();
+
+    std::cout << "unit_tests passed" << std::endl;
     return 0;
 }
--- tests/test_tree_printer.cpp
+++
@@ -1,1 +0,0 @@
-// Placeholder for TreePrinter unit tests.
