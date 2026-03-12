9be4d92f605f32e56d8bf6b2c3b03a0704e384be
pashtetmak-lab <pashtetmak@gmail.com>
2026-03-12 19:47:33 +0300

    Update test_size_calculator.cpp

--- tests/test_size_calculator.cpp
+++ tests/test_size_calculator.cpp
@@ -1,24 +1,38 @@
-#include "SizeCalculator.h"
-#include "FsNode.h"
+#include "../include/SizeCalculator.h"
+#include "../include/FsNode.h"
 
 #include <cassert>
 #include <filesystem>
+#include <fstream>
 #include <memory>
 
 void TestCalculateDirectorySize()
 {
     namespace fs = std::filesystem;
 
-    auto root = std::make_unique<FsNode>(FsNode::DirInfo{fs::path("root"), 0});
+    fs::path dir = "test_tmp_dir";
+    fs::create_directory(dir);
 
-    auto file1 = std::make_unique<FsNode>(FsNode::FileInfo{fs::path("a.txt"), 100});
-    auto file2 = std::make_unique<FsNode>(FsNode::FileInfo{fs::path("b.txt"), 200});
+    fs::path file1 = dir / "a.txt";
+    fs::path file2 = dir / "b.txt";
 
-    root->addChild(std::move(file1));
-    root->addChild(std::move(file2));
+    std::ofstream(file1) << "12345";
+    std::ofstream(file2) << "1234567890";
+
+
+    auto root = std::make_unique<FsNode>(FsNode::DirInfo{dir});
+
+    root->addChild(std::make_unique<FsNode>(
+        FsNode::FileInfo{file1}));
+
+    root->addChild(std::make_unique<FsNode>(
+        FsNode::FileInfo{file2}));
 
     SizeCalculator calc;
-    calc.Calculate(*root);
 
-    assert(root->directory().size_bytes == 300);
+    auto size = calc.Calculate(*root);
+
+    assert(size == 15);
+
+    fs::remove_all(dir);
 }
