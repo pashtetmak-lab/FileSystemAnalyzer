6fe4a58253e27e239adf02aeee2af990e30c5e0b
pashtetmak-lab <pashtetmak@gmail.com>
2026-03-14 09:20:17 +0300

    Update FsNode.cpp

--- src/FsNode.cpp
+++ src/FsNode.cpp
@@ -1,34 +1,40 @@
 #include "../include/FsNode.h"
 #include <stdexcept>
 
+// std::move используется, чтобы переместить объект а не копировать его.
 FsNode::FsNode(FileInfo file)
     : data_(std::move(file))
 {
 }
-
+// Аналогично предыдущему но теперь variant хранит DirInfo.
 FsNode::FsNode(DirInfo dir)
     : data_(std::move(dir))
 {
 }
-
+// std::holds_alternative проверяет какой именно тип сейчас хранится внутри std::variant.
 bool FsNode::isFile() const
 {
     return std::holds_alternative<FileInfo>(data_);
 }
 
+// Проверяет является ли узел директорией.
 bool FsNode::isDirectory() const
 {
     return std::holds_alternative<DirInfo>(data_);
 }
 
+// Возвращает ссылку на FileInfo. Если узел не является файлом выбрасывается исключение.
+// Это защищает от неправильного использования класса.
 FsNode::FileInfo& FsNode::file()
 {
     if (!isFile())
         throw std::logic_error("Not a file");
 
+    // std::get извлекает конкретный тип из variant
     return std::get<FileInfo>(data_);
 }
 
+// Возвращает ссылку на DirInfo.
 FsNode::DirInfo& FsNode::directory()
 {
     if (!isDirectory())
@@ -37,11 +43,14 @@ FsNode::DirInfo& FsNode::directory()
     return std::get<DirInfo>(data_);
 }
 
+// Добавляет дочерний узел в дерево children_ хранит unique_ptr через std::move.
 void FsNode::addChild(std::unique_ptr<FsNode> child)
 {
     children_.push_back(std::move(child));
 }
 
+
+// Возвращается const-ссылка, чтобы: не копировать vector
 const std::vector<std::unique_ptr<FsNode>>& FsNode::children() const
 {
     return children_;
@@ -51,6 +60,7 @@ const FsNode::FileInfo& FsNode::file() const
 {
     if (!isFile())
         throw std::logic_error("Not a file");
+
     return std::get<FileInfo>(data_);
 }
 
@@ -58,5 +68,6 @@ const FsNode::DirInfo& FsNode::directory() const
 {
     if (!isDirectory())
         throw std::logic_error("Not a directory");
+
     return std::get<DirInfo>(data_);
 }
